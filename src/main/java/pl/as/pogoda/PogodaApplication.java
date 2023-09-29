94423cba14a26a9a232c2a97ec64a7c7295856a7
rbzf <anna.sadkowska@gmail.com>
2023-09-29 06:37:29 +0200

    Utworzenie projektu

---
+++ src/main/java/pl/as/pogoda/PogodaApplication.java
@@ -0,0 +1,13 @@
+package pl.as.pogoda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PogodaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PogodaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/as/pogoda/PogodaApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.as.pogoda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PogodaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
